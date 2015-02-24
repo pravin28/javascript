642733e60fc32fb07ce1f90b3e9ce1370014b5e4
pravin28 <meetpravinpk@gmail.com>
2015-02-24 18:24:37 +0530

    first

---
+++ js 5/jb.js
@@ -0,0 +1,25 @@
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+var age=prompt("whats ur age");
+if(age<13){
+    console .log("u r allowed to play but we r not responsible to any");
+    
+    }
+    else{
+        console.log("boooyeah...! u r in...");
+        }
+        console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+var userAnswer=prompt("Do you want to race Bieber on stage?");
+if(userAnswer==="yes")
+{
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");}
+    else{
+        console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+        }
+        var feedback=prompt("rate the game out of 10");
+        if(feedback>8)
+        {
+            console.log("Thank you! We should race at the next concert!");}
+            else{
+                console.log("I'll keep practicing coding and racing.");}
