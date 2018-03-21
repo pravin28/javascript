450d68215f848f59d7448f724bbcb794541cf3fe
pravin28 <meetpravinpk@gmail.com>
2018-03-21 11:45:58 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,14 +3,14 @@ class posnegzero
 {
 public static void main (String []s)
 {
-int num;
+int n;
 Scanner sc=newScanner(System.in);
 System.out.print("Enter the integer number":);
-num=sc.next Int();
+n=sc.next Int();
 if(num>0)
 {
  System.out.println(number +"is POSITIVE NUMBER");
-else if(num<0)
+else if(n<0)
  System.out.println(number+"is NEGATIVE NUMBER");
 else
  System.out.println("IT's ZERO");
