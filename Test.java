0396a38ee6355e0e891de64c02a3f06f6873c4b9
pravin28 <meetpravinpk@gmail.com>
2018-03-21 11:42:01 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,18 @@
-Hello
+import java.util.*;
+class posnegzero
+{
+public static void main (String []s)
+{
+int num;
+Scanner sc=newScanner(System.in);
+System.out.print("Enter the integer number":);
+num=sc.next Int();
+if(num>0)
+{
+ System.out.println(number +"is POSITIVE NUMBER");
+else if(num<0)
+ System.out.println(number+"is NEGATIVE NUMBER");
+else
+ System.out.println("IT's ZERO");
+}
+}
